69c5cf86b4801427e5f54f84a02204d5e0c5202d
Dionird <dionird20@gmail.com>
2018-07-07 01:04:17 +0200

    InterCommit

---
+++ PaquetePonyProject/Assets/Script/PlayerHealth.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour {
+
+    [SerializeField] private int currentHealth;
+
+    private PlayerStats _playerStats;
+
+	// Use this for initialization
+	void Awake ()
+    {
+        _playerStats = gameObject.GetComponent<PlayerStats>();
+        currentHealth = _playerStats.GetCurrentMaxHealth();
+	}
+	
+	public void OnMaxHealthModified(int lastMaxHealth)
+    {
+        currentHealth = (currentHealth * _playerStats.GetCurrentMaxHealth()) / lastMaxHealth;
+    }
+}
