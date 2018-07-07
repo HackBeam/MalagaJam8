73e2344438a07b160bdabc0e31c8c865a1196157
Gregorio Carvajal Exposio <gregoaplweb@gmail.com>
2018-07-07 14:34:52 +0200

    Shots working
    Now shots moves and collides with other objects

--- PaquetePonyProject/Assets/Scripts/Shoot.cs
+++ PaquetePonyProject/Assets/Scripts/Shoot.cs
@@ -1,8 +1,48 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Shoot : MonoBehaviour {
+public class Shoot : MonoBehaviour
+{
 
-	public int damage;
+    [HideInInspector] public int damage;
+
+    [SerializeField] private float lifeTime;
+    [SerializeField] private float speed;
+    [SerializeField] private LayerMask collidableLayers;
+
+    private WaitForSeconds lifeWait;
+
+    private void Awake()
+    {
+        lifeWait = new WaitForSeconds(lifeTime);
+    }
+
+    private void Update()
+    {
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+
+    private void OnEnable()
+    {
+        StartCoroutine(LifetimeCountdown());
+    }
+
+    private IEnumerator LifetimeCountdown()
+    {
+        yield return lifeWait;
+        this.gameObject.SetActive(false);
+    }
+
+    private void OnDisable()
+    {
+        StopAllCoroutines();
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (((1 << other.gameObject.layer) & collidableLayers) != 0)
+            gameObject.SetActive(false);
+    }
 }
