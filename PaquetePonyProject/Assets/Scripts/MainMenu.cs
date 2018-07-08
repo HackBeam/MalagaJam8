d37821b7a00c2cea9dd468dc761f315b6b3c5f17
Dionird <dionird20@gmail.com>
2018-07-08 14:40:06 +0200

    Init Menu

---
+++ PaquetePonyProject/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject initobj;
+
+	void Start ()
+    {
+        StartCoroutine(MainMenuCoro());
+	}
+
+    private IEnumerator MainMenuCoro()
+    {
+
+        while (! Input.anyKeyDown)
+        {
+            yield return null;
+        }
+
+        initobj.SetActive(false);
+
+        while (!Input.anyKeyDown)
+        {
+            yield return null;
+        }
+
+        SceneManager.LoadScene("Jny");
+    }
+}
